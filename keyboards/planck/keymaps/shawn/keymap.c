fd7c40f411fe7d881e7aab8dfaae78bdef6c735e
Shawn Taylor <shawn@mapsandapps.net>
2017-11-21 21:31:17 -0500

    Adds Shawn's Planck layout

---
+++ keyboards/planck/keymaps/shawn/keymap.c
@@ -0,0 +1,94 @@
+#include "planck.h"
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+
+// Fillers to make layering more clear
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Qwerty
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |   '  |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Brite| Ctrl |  Alt | GUI  |Lower |    Space    |Raise |  GUI |  Alt | Ctrl | Shift|
+ * `-----------------------------------------------------------------------------------'
+ */
+[0] = {
+  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
+  {BL_STEP, KC_LCTL, KC_LALT, KC_LGUI, M(1),    KC_SPC,  KC_SPC,   M(2),   KC_RGUI, KC_RALT, KC_RCTL, KC_RSFT}
+},
+
+/* Lower/Orange
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   `  |   &  |   *  |      |      |      |   7  |   8  |   9  |   -  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |      |   $  |   %  |   ^  |   <  |   >  |   4  |   5  |   6  |   +  |   \  |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |   !  |   @  |   #  |      |      |   1  |   2  |   3  |Enter |   =  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |Reset |      |      |      |      |    Space    |   0  |   0  |   .  |Enter |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[1] = {
+  {KC_TAB,  KC_GRV,  KC_AMPR, KC_ASTR, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_PMNS, KC_BSPC},
+  {KC_ESC,  _______, KC_DLR,  KC_PERC, KC_CIRC, KC_LT,   KC_GT,   KC_4,    KC_5,    KC_6,    KC_PPLS, KC_BSLS},
+  {_______, _______, KC_EXLM, KC_AT,   KC_HASH, _______, _______, KC_1,    KC_2,    KC_3,    KC_PENT, KC_EQL },
+  {RESET,   _______, _______, _______, _______, KC_SPC,  KC_SPC,  KC_0,    KC_0,    KC_DOT,  KC_PENT, _______}
+},
+
+/* Raise/Blue
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   ~  |      |      |      |   (  |   )  |   /  |  Up  |   *  |   _  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |      |      |      |      |   {  |   }  | Left | Down |Right |      |   |  |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |   [  |   ]  |      |      |      |      |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |Reset |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[2] = {
+  {KC_TAB,  KC_TILD, _______, _______, _______, KC_LPRN, KC_RPRN, KC_PSLS, KC_UP,   KC_PAST, KC_UNDS, KC_BSPC},
+  {KC_ESC,  _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_PIPE},
+  {_______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______, _______, KC_ENT },
+  {RESET,   _______, _______, _______, _______, KC_SPC,  KC_SPC,  _______, _______, _______, _______, _______}
+},
+
+
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  switch(id) {
+    case 1:
+      if (record->event.pressed) {
+        layer_on(1);
+      } else {
+        layer_off(1);
+      }
+      break;
+    case 2:
+      if (record->event.pressed) {
+        layer_on(2);
+      } else {
+        layer_off(2);
+      }
+      break;
+  }
+  return MACRO_NONE;
+};
---
+++ keyboards/planck/keymaps/shawn/readme.md
@@ -0,0 +1,13 @@
+# A Planck layout with numpad and inverted-T home row arrows
+
+On [Keyboard Layout Editor](http://www.keyboard-layout-editor.com/#/gists/57cf805069862b99fe1059e1b0988e0a)
+
+![Keyboard](https://i.imgur.com/NdWX4jH.png)
+
+To flash firmware:
+
+    make planck:shawn:dfu
+
+The press the "reset" key combination
+
+If that doesn't work, press the physical button on the back of the PCB, then run the `make` command
