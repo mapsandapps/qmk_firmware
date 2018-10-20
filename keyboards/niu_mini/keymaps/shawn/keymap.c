f24f5b9f426903ba08deb5e84a7612b6a8362b5c
Shawn Taylor <shawn@mapsandapps.net>
2018-10-20 10:45:51 -0400

    RGB for niu mini

--- keyboards/niu_mini/keymaps/shawn/keymap.c
+++ keyboards/niu_mini/keymaps/shawn/keymap.c
@@ -25,7 +25,7 @@ enum keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	/* Layer 0
+	/* Qwerty
 	 * ,-----------------------------------------------------------------------------------.
 	 * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
 	 * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -33,14 +33,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * |------+------+------+------+------+------|------+------+------+------+------+------|
 	 * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
-	 * |      | Tab  | Alt  | GUI  |Lower |    Space    |Raise | GUI  | Alt  | Ctrl |  LR  |
+	 * |      | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | GUI  | Alt  | Ctrl |  LR  |
 	 * `-----------------------------------------------------------------------------------'
 	 */
 	[_QWERTY] = LAYOUT_planck_mit(
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
 		KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-		_______, KC_TAB, KC_LALT, KC_LGUI, LOWER,     KC_SPC,      RAISE, KC_RGUI, KC_RALT, KC_RCTRL,LR
+		_______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,     KC_SPC,      RAISE, KC_RGUI, KC_RALT, KC_RCTRL,LR
   ),
 
   /* Lower
@@ -51,14 +51,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |------+------+------+------+------+------|------+------+------+------+------+------|
   * |      |      |   !  |   @  |   #  |      |      |   1  |   2  |   3  |Enter |   =  |
   * |------+------+------+------+------+------+------+------+------+------+------+------|
-  * |      |      |      |      |      |      |      |   0  |   0  |   .  |Enter |      |
+  * |      |      |      |      |      |    Space    |   0  |   0  |   .  |Enter |      |
   * `-----------------------------------------------------------------------------------'
   */
   [_LOWER] = LAYOUT_planck_mit(
   _______, KC_GRV,  KC_AMPR, KC_ASTR, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_PMNS, _______,
   _______, _______, KC_DLR,  KC_PERC, KC_CIRC, KC_LT,   KC_GT,   KC_4,    KC_5,    KC_6,    KC_PPLS, KC_BSLS,
   _______, _______, KC_EXLM, KC_AT,   KC_HASH, _______, _______, KC_1,    KC_2,    KC_3,    KC_PENT, KC_EQL ,
-  _______, _______, _______, _______, _______,    KC_SPC,        KC_0,    KC_0,    KC_DOT,  KC_PENT, _______
+  _______, _______, _______, _______, _______,      KC_SPC,      KC_0,    KC_0,    KC_DOT,  KC_PENT, _______
   ),
 
 	/* Raise
@@ -69,14 +69,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* |------+------+------+------+------+------|------+------+------+------+------+------|
 	* |      |BL_TOG|BL_STEP|     |      |   [  |   ]  |      |      |      |      |      |
 	* |------+------+------+------+------+------+------+------+------+------+------+------|
-	* |      |      |      |      |      |      |      |      |      |      |      |      |
+	* |      |      |      |      |      |    Space    |      |      |      |      |      |
 	* `-----------------------------------------------------------------------------------'
 	*/
   [_RAISE] = LAYOUT_planck_mit(
   _______, KC_TILD, _______, _______, _______, KC_LPRN, KC_RPRN, KC_PSLS, KC_UP,   KC_PAST, KC_UNDS, KC_BSPC,
   _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, KC_LCBR, KC_RCBR, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_PIPE,
   _______, BL_TOGG, BL_STEP, _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______, _______, KC_ENT ,
-  _______, _______, _______, _______, _______,    KC_SPC,        _______, _______, _______, _______, _______
+  _______, _______, _______, _______, _______,      KC_SPC,      _______, _______, _______, _______, _______
   ),
 
 	/* LR
@@ -87,14 +87,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* |------+------+------+------+------+------|------+------+------+------+------+------|
 	* |      |      |      |      |      |      |   0  |   1  |   2  |   3  |   4  |   5  |
 	* |------+------+------+------+------+------+------+------+------+------+------+------|
-	* |      |      |      |      |      |      |      |      |      |      |      |Qwerty|
+	* |      |      |      |      |      |    Space    |      |      |      |      |Qwerty|
 	* `-----------------------------------------------------------------------------------'
 	*/
-	[_LR] = LAYOUT_ortho_4x12(
+	[_LR] = LAYOUT_planck_mit(
 		_______, _______, _______, _______, _______, _______, M(0),    M(1),    KC_UP,   KC_P,    KC_U,    KC_X,
 		_______, _______, _______, _______, _______, _______, KC_G,    KC_LEFT, KC_DOWN, KC_RGHT, M(2),    M(3),
 		_______, _______, _______, _______, _______, _______, KC_0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
-		_______, _______, _______, _______, _______, KC_SPC,  KC_SPC,  _______, _______, _______, _______, QWERTY
+		_______, _______, _______, _______, _______,       KC_SPC,     _______, _______, _______, _______, QWERTY
 	),
 
   /* Adjust (Lower + Raise)
@@ -108,16 +108,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      |      |      |             |      |      |      |      |      |
    * `-----------------------------------------------------------------------------------'
    */
-  [_ADJUST] = LAYOUT_ortho_4x12(
+  [_ADJUST] = LAYOUT_planck_mit(
     _______, RESET,   _______, _______, _______, _______, RESET,   _______, _______, _______, _______, KC_DEL ,
     _______, _______, _______, _______, _______, AG_NORM, AG_SWAP, QWERTY,  _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
   )
 };
 
 
 void matrix_init_user(void) {
+	rgblight_mode(2);
+	rgblight_sethsv(22, 255, 255);
 }
 
 void matrix_scan_user(void) {
@@ -129,6 +131,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         print("mode just switched to qwerty and this is a huge string\n");
         set_single_persistent_default_layer(_QWERTY);
+				rgblight_mode(1);
+				rgblight_sethsv(22, 255, 255);
+				// rgblight_sethsv_at(0, 0, 255, 0);
+				// rgblight_sethsv_at(0, 0, 255, 1);
+				// rgblight_sethsv_at(0, 0, 255, 2);
+				// rgblight_sethsv_at(22, 255, 255, 3);
+				// rgblight_sethsv_at(22, 255, 255, 4);
+				// rgblight_sethsv_at(22, 255, 255, 5);
+				// rgblight_sethsv_at(22, 255, 255, 6);
+				// rgblight_sethsv_at(247, 0, 0, 7);
+				// rgblight_sethsv_at(247, 0, 0, 8);
+				// rgblight_sethsv_at(247, 0, 0, 9);
+				// rgblight_sethsv_at(22, 255, 255, 10);
+				// rgblight_sethsv_at(22, 255, 255, 11);
+				// rgblight_sethsv_at(22, 255, 255, 12);
+				// rgblight_sethsv_at(22, 255, 255, 13);
       }
       return false;
       break;
@@ -155,6 +173,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case LR:
       if (record->event.pressed) {
         set_single_persistent_default_layer(_LR);
+				rgblight_mode(2);
+				rgblight_sethsv(206, 60, 220);
       }
       return false;
       break;
