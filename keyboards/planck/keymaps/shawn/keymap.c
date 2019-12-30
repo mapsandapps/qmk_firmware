26b86a06a9fc3e7267d77b3d50c2a7b6326f329a
Shawn Taylor <shawn@mapsandapps.net>
2019-12-30 10:28:00 -0500

    Make LED key less fatfingerable; add volume controls

--- keyboards/planck/keymaps/shawn/keymap.c
+++ keyboards/planck/keymaps/shawn/keymap.c
@@ -19,14 +19,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl |  Alt | GUI  |Lower |    Space    |Raise |  GUI |  Alt | Ctrl | Shift|
+ * |      | Ctrl |  Alt | GUI  |Lower |    Space    |Raise |  GUI |VolDn |VolUp | Mute |
  * `-----------------------------------------------------------------------------------'
  */
 [0] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {BL_STEP, KC_LCTL, KC_LALT, KC_LGUI, M(1),    KC_SPC,  KC_SPC,   M(2),   KC_RGUI, KC_RALT, KC_RCTL, KC_RSFT}
+  {_______, KC_LCTL, KC_LALT, KC_LGUI, M(1),    KC_SPC,  KC_SPC,   M(2),   KC_RGUI, KC__VOLDOWN, KC__VOLUP, KC__MUTE}
 },
 
 /* Lower/Orange
@@ -37,14 +37,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |   !  |   @  |   #  |      |      |   1  |   2  |   3  |Enter |   =  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Reset |      |      |      |      |    Space    |   0  |   0  |   .  |Enter |      |
+ * |Brite |      |      |      |      |    Space    |   0  |   0  |   .  |Enter |      |
  * `-----------------------------------------------------------------------------------'
  */
 [1] = {
   {KC_GRV,  _______, KC_AMPR, KC_ASTR, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_PMNS, KC_BSPC},
   {KC_ESC,  _______, KC_DLR,  KC_PERC, KC_CIRC, KC_LT,   KC_GT,   KC_4,    KC_5,    KC_6,    KC_PPLS, KC_BSLS},
   {_______, _______, KC_EXLM, KC_AT,   KC_HASH, _______, _______, KC_1,    KC_2,    KC_3,    KC_PENT, KC_EQL },
-  {RESET,   _______, _______, _______, _______, KC_SPC,  KC_SPC,  KC_0,    KC_0,    KC_DOT,  KC_PENT, _______}
+  {BL_STEP,   _______, _______, _______, _______, KC_SPC,  KC_SPC,  KC_0,    KC_0,    KC_DOT,  KC_PENT, _______}
 },
 
 /* Raise/Blue
--- keyboards/planck/keymaps/shawn/readme.md
+++ keyboards/planck/keymaps/shawn/readme.md
@@ -2,9 +2,24 @@
 
 On [Keyboard Layout Editor](http://www.keyboard-layout-editor.com/#/gists/57cf805069862b99fe1059e1b0988e0a)
 
-![Keyboard](https://i.imgur.com/NdWX4jH.png)
+![Keyboard](https://i.imgur.com/5DumGes.png)
 
-To flash firmware:
+[Install QMK](https://docs.qmk.fm/#/getting_started_build_tools?id=macos)
+
+```
+cd qmk_firmware
+brew tap osx-cross/avr
+brew tap PX4/homebrew-px4
+brew update
+brew install avr-gcc@8
+brew link --force avr-gcc@8
+brew install dfu-programmer
+brew install dfu-util
+brew install gcc-arm-none-eabi
+brew install avrdude
+```
+
+To flash firmware (after installing QMK):
 
     cd qmk_firmware
     make planck:shawn:dfu
