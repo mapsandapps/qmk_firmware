1e7d4625c9261c05d76139ac81959e27cb513f8f
Shawn Taylor <shawn@mapsandapps.net>
2020-02-07 09:43:42 -0500

    Update Let's Split: remove LR layer, add volume controls, and adjust a few other things

--- keyboards/lets_split/keymaps/shawn/keymap.c
+++ keyboards/lets_split/keymaps/shawn/keymap.c
@@ -11,15 +11,11 @@ extern keymap_config_t keymap_config;
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
-#define _LR 3
-#define _ADJUST 16
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
-  RAISE,
-  LR,
-  ADJUST,
+  RAISE
 };
 
 // Fillers to make layering more clear
@@ -36,19 +32,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Adjust| Ctrl |  Alt |  GUI |Lower |Space |Space |Raise |  GUI |  Alt |Adjust|  LR  |
+ * |      | Ctrl |  Alt |  GUI |Lower |Space |Space |Raise |  GUI |  Alt | Ctrl |Shift |
  * `-----------------------------------------------------------------------------------'
  */
-[_QWERTY] = KEYMAP( \
+[_QWERTY] = LAYOUT_ortho_4x12( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  ADJUST,  KC_LCTRL,KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, ADJUST,  LR       \
+  _______, KC_LCTRL,KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_RCTRL, KC_RSFT \
 ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |      |   `  |   &  |   *  |      |      |      |   7  |   8  |   9  |   -  |      |
+ * |  `   |      |   &  |   *  |      |      |      |   7  |   8  |   9  |   -  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |   $  |   %  |   ^  |   <  |   >  |   4  |   5  |   6  |   +  |   \  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -57,8 +53,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |   0  |   0  |   .  |Enter |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_LOWER] = KEYMAP( \
-  _______, KC_GRV,  KC_AMPR, KC_ASTR, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_PMNS, _______, \
+[_LOWER] = LAYOUT_ortho_4x12( \
+  KC_GRV,  _______, KC_AMPR, KC_ASTR, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_PMNS, _______, \
   _______, _______, KC_DLR,  KC_PERC, KC_CIRC, KC_LT,   KC_GT,   KC_4,    KC_5,    KC_6,    KC_PPLS, KC_BSLS, \
   _______, _______, KC_EXLM, KC_AT,   KC_HASH, _______, _______, KC_1,    KC_2,    KC_3,    KC_PENT, KC_EQL , \
   _______, _______, _______, _______, _______, KC_SPC,  KC_SPC,  KC_0,    KC_0,    KC_DOT,  KC_PENT, _______  \
@@ -66,56 +62,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |      |   ~  |      |      |      |   (  |   )  |   /  |  Up  |   *  |   _  |      |
+ * |   ~  |      |      |      |      |   (  |   )  |   /  |  Up  |   *  |   _  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |      |      |   {  |   }  | Left | Down |Right |      |   |  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |   [  |   ]  |      |      |      |      |      |
+ * |      |Reset |      |      |      |   [  |   ]  |      |      |      |      |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |      |      |      |VolDn |VolUp | Mute |
  * `-----------------------------------------------------------------------------------'
  */
-[_RAISE] = KEYMAP( \
-  _______, KC_TILD, _______, _______, _______, KC_LPRN, KC_RPRN, KC_PSLS, KC_UP,   KC_PAST, KC_UNDS, KC_BSPC, \
-  _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_PIPE, \
-  _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______, _______, KC_ENT , \
-  _______, _______, _______, _______, _______, KC_SPC,  KC_SPC,  _______, _______, _______, _______, _______  \
-),
-
-/* LR
- * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |      |  Up  |   P  |   U  |   X  |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |   G  | Left | Down |Right |      |      |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |   0  |   1  |   2  |   3  |   4  |   5  |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |Qwerty|
- * `-----------------------------------------------------------------------------------'
- */
-[_LR] = KEYMAP( \
-  _______, _______, _______, _______, _______, _______, M(0),    M(1),    KC_UP,   KC_P,    KC_U,    KC_X,    \
-  _______, _______, _______, _______, _______, _______, KC_G,    KC_LEFT, KC_DOWN, KC_RGHT, M(2),    M(3),    \
-  _______, _______, _______, _______, _______, _______, KC_0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    \
-  _______, _______, _______, _______, _______, KC_SPC,  KC_SPC,  _______, _______, _______, _______, QWERTY   \
-),
-
-/* Adjust
- * ,-----------------------------------------------------------------------------------.
- * |      |Reset |      |      |      |      |Reset |      |      |      |      |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |AGnorm|AGswap|Qwerty|      |      |      |      |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
-[_ADJUST] =  KEYMAP( \
-  _______, RESET,   _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, AG_NORM, AG_SWAP, QWERTY,  _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+[_RAISE] = LAYOUT_ortho_4x12( \
+  KC_TILD, _______, _______, _______, _______, KC_LPRN, KC_RPRN, KC_PSLS, KC_UP,   KC_PAST,     KC_UNDS,   KC_BSPC, \
+  _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_LEFT, KC_DOWN, KC_RGHT,     _______,   KC_PIPE, \
+  _______, RESET,   _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______,     _______,   KC_ENT , \
+  _______, _______, _______, _______, _______, KC_SPC,  KC_SPC,  _______, _______, KC__VOLDOWN, KC__VOLUP, KC__MUTE \
 )
 
 
@@ -162,37 +122,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
       break;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case LR:
-      if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(tone_colemak);
-        #endif
-        persistent_default_layer_set(1UL<<_LR);
-      }
-      return false;
-      break;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
       }
       return false;
       break;
--- keyboards/lets_split/keymaps/shawn/readme.md
+++ keyboards/lets_split/keymaps/shawn/readme.md
@@ -1,10 +1,27 @@
 # A Let's Split layout with numpad and inverted-T home row arrows
 
-To flash firmware:
+On [Keyboard Layout Editor](http://www.keyboard-layout-editor.com/#/gists/a7d5417f7fd79a85605b1b5dbe43dcc2)
 
-    brew install avrdude
+![Keyboard](https://i.imgur.com/vmZS9IG.png)
+
+```
+cd qmk_firmware
+brew tap osx-cross/avr
+brew tap PX4/homebrew-px4
+brew update
+brew install avr-gcc@8
+brew link --force avr-gcc@8
+brew install dfu-programmer
+brew install dfu-util
+brew install gcc-arm-none-eabi
+brew install avrdude
+```
+
+To flash firmware (after installing packages):
+
+    cd qmk_firmware
     make lets_split:shawn:avrdude
 
 Then press the reset key combination
 
-Right is master, so plug that side in after flashing firmware.
+Right is master, so plug that side in after flashing firmware (can be changed in config.h).
